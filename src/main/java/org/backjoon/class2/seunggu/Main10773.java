51f15f02d2a4c511760455e05e49797e75a21c0e
leeseunggu <leesg6107@gmail.com>
2023-04-21 23:55:34 +0900

    feat: solve 10773 제로 (실버 4)

---
+++ src/main/java/org/backjoon/class2/seunggu/Main10773.java
@@ -0,0 +1,37 @@
+package org.backjoon.class2.seunggu;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * 제로 ( 실버 4)
+ */
+public class Main10773 {
+
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            int count = Integer.parseInt(reader.readLine());
+            Deque<Integer> deque = new ArrayDeque<>();
+            for (int i = 0; i < count; i++) {
+                int number = Integer.parseInt(reader.readLine());
+                if (number == 0 && !deque.isEmpty()) {
+                    deque.pop();
+                } else {
+                    deque.push(number);
+                }
+            }
+            int answer = 0;
+            while (!deque.isEmpty()) {
+                answer += deque.pop();
+            }
+
+            System.out.println(answer);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
