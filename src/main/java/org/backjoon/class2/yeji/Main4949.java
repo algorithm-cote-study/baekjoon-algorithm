aca2e1a9f5358f2e7c3792e6e9946e096abeaf93
yeji.jo <choyeji1591@gmail.com>
2023-04-22 00:54:46 +0900

    feat : class2 균형잡힌 세상 풀이

---
+++ src/main/java/org/backjoon/class2/yeji/Main4949.java
@@ -0,0 +1,60 @@
+package org.backjoon.class2.yeji;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+// 균형잡힌 세상
+public class Main4949 {
+
+    public static void main ( String[] args ) {
+        try ( InputStreamReader in = new InputStreamReader( System.in ); BufferedReader br = new BufferedReader( in ) ) {
+
+            System.out.println( solution( br ) );
+
+        } catch ( IOException ex ) {
+            ex.printStackTrace();
+        }
+    }
+
+    static String solution ( BufferedReader br ) throws IOException {
+        StringBuilder answer = new StringBuilder();
+        String line;
+        while ( !( line = br.readLine() ).equals( "." ) ) {
+            String[] input = line.split( "" );
+            if ( isCollectParentheses( input ) ) {
+                answer.append( "yes" ).append( "\n" );
+            } else {
+                answer.append( "no" ).append( "\n" );
+            }
+        }
+
+        return answer.toString();
+    }
+
+    private static boolean isCollectParentheses ( String[] input ) {
+
+        Deque<String> stack = new ArrayDeque<>();
+        for ( String in : input ) {
+            if ( in.equals( "(" ) || in.equals( "[" ) ) {
+                stack.push( in );
+            } else if ( in.equals( "]" ) ) {
+                if ( !stack.isEmpty() && stack.peek().equals( "[" ) ) {
+                    stack.pop();
+                } else {
+                    stack.push( in );
+                }
+            } else if ( in.equals( ")" ) ) {
+                if ( !stack.isEmpty() && stack.peek().equals( "(" ) ) {
+                    stack.pop();
+                } else {
+                    stack.push( in );
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
