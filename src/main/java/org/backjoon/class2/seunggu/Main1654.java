e3a0b92e0a602df21370a4a191a9293d7bb77a0d
leeseunggu <leesg6107@gmail.com>
2023-04-23 23:10:16 +0900

    feat: resolve 1654 ( 랜선 자르기 )

---
+++ src/main/java/org/backjoon/class2/seunggu/Main1654.java
@@ -0,0 +1,45 @@
+package org.backjoon.class2.seunggu;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main1654 {
+
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            long rt = solution(reader);
+            System.out.println(rt);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static int solution(BufferedReader reader) throws IOException {
+        String[] lines = reader.readLine().split(" ");
+        int cnt = Integer.parseInt(lines[0]);
+        int count = Integer.parseInt(lines[1]);
+        int[] array = new int[cnt];
+        for (int i = 0; i < cnt; i++) {
+            array[i] = Integer.parseInt(reader.readLine());
+        }
+        int lt = 1;
+        int rt = Arrays.stream(array).max().getAsInt();
+        while (lt <= rt) {
+            int total = 0;
+            int half = (lt + rt) / 2;
+            for (int j : array) {
+                total += (j / half);
+            }
+            if (total >= count) {
+                lt = half + 1;
+            } else {
+                rt = half - 1;
+            }
+        }
+        return rt;
+    }
+
+
+}
