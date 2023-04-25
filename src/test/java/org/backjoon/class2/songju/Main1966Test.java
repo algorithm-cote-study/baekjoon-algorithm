8f0d322289c43fe6a248a77a6c593a9cc12f2cbe
songjuAhn <ggb06104@naver.com>
2023-04-25 17:33:42 +0900

    test : class2 프린터 큐 테스트케이스 추가

---
+++ src/test/java/org/backjoon/class2/songju/Main1966Test.java
@@ -0,0 +1,24 @@
+package org.backjoon.class2.songju;
+
+import org.backjoon.common.TestFileUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName( "프린터 큐 - 송주" )
+class Main1966Test {
+
+    @Test
+    @DisplayName( "프린터 큐 케이스 1" )
+    void test_case_1 () throws Exception {
+        BufferedReader reader = TestFileUtil.getReader( this.getClass(), "testcase/class2/1966/test_case1.txt" );
+        String solution = Main1966.solution( reader );
+        assertEquals( "1\n" +
+                "2\n" +
+                "5\n", solution );
+    }
+
+}
