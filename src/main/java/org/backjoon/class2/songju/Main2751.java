1a295eb7e84ef4753cdf8aaf39396e6e0d7903c1
songjuAhn <ggb06104@naver.com>
2023-04-26 12:15:09 +0900

    feat : class2 수 정렬하기 2 풀이

---
+++ src/main/java/org/backjoon/class2/songju/Main2751.java
@@ -0,0 +1,36 @@
+package org.backjoon.class2.songju;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+//수 정렬하기 2
+public class Main2751 {
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println(solution(br));
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static String solution(BufferedReader br) throws IOException {
+        StringBuilder result = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        for(int i =  0 ; i < N ; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        for(int num : arr) {
+            result.append(num).append("\n");
+        }
+        return result.toString();
+    }
+}
