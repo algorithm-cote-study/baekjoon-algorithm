79401164a1b4f25b68bd17724edad1e2940ff231
yeji.jo <choyeji1591@gmail.com>
2023-04-26 16:21:49 +0900

    test : class3 피보나치 테스트케이스 추가

---
+++ src/test/java/org/backjoon/class3/yeji/Main1003Test.java
@@ -0,0 +1,40 @@
+package org.backjoon.class3.yeji;
+
+import static org.backjoon.class3.yeji.Main1003.solution;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import org.backjoon.common.TestFileUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName( "피보나치 함수 - 예지" )
+class Main1003Test {
+
+    @Test
+    @DisplayName( "피보나치 함수 케이스1" )
+    void test_case_1 () throws IOException {
+        // given토
+        int[][] compare = { { 1, 0 }, { 0, 1 }, { 1, 2 } };
+        BufferedReader reader = TestFileUtil.getReader( this.getClass(), "testcase/class3/1003/test_case1.txt" );
+        // when
+        int[][] solution = solution( reader );
+
+        // then
+        assertArrayEquals( compare, solution );
+    }
+
+    @Test
+    @DisplayName( "피보나치 함수 케이스2" )
+    void test_case_2 () throws IOException {
+        // given토
+        int[][] compare = { { 5, 8 }, { 10946, 17711 }, };
+        BufferedReader reader = TestFileUtil.getReader( this.getClass(), "testcase/class3/1003/test_case2.txt" );
+        // when
+        int[][] solution = solution( reader );
+
+        // then
+        assertArrayEquals( compare, solution );
+    }
+}
