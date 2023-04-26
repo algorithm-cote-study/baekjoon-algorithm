3a9f518a28e4674181904d0bcfeb3981b7a30329
songjuAhn <ggb06104@naver.com>
2023-04-26 13:42:09 +0900

    feat : class2 영화감독 숌 풀이

---
+++ src/main/java/org/backjoon/class2/songju/Main1436.java
@@ -0,0 +1,31 @@
+package org.backjoon.class2.songju;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//영화감독 숌
+public class Main1436 {
+    static int num = 666;
+    static int cnt =0;
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println(solution(br));
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static int solution(BufferedReader br) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        int endNum = 666;
+        int cnt = 0;
+        while(true){
+            if(String.valueOf(endNum).contains("666")) cnt++;
+            if(cnt == N) break;
+            endNum++;
+        }
+        return endNum;
+    }
+}
