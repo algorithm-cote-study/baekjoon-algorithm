e391b09c077a94c913c94636e32bde28761554ee
leeseunggu <leesg6107@gmail.com>
2023-05-01 19:45:28 +0900

    fix: 가독성 측면에서 전역 변수로 변경

--- src/main/java/org/backjoon/class3/seunggu/Main1260.java
+++ src/main/java/org/backjoon/class3/seunggu/Main1260.java
@@ -12,6 +12,10 @@ import java.util.LinkedList;
  */
 public class Main1260 {
 
+    private static boolean[] visited;
+    private static boolean[][] board;
+    private static StringBuilder stringBuilder;
+
     public static void main(String[] args) {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             System.out.println(solution(reader));
@@ -22,22 +26,22 @@ public class Main1260 {
 
     static String solution(BufferedReader reader) throws IOException {
         int[] array = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        boolean[][] board = new boolean[array[0] + 1][array[0] + 1];
+        board = new boolean[array[0] + 1][array[0] + 1];
         for (int i = 0; i < array[1]; i++) {
             int[] arr = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
             board[arr[0]][arr[1]] = board[arr[1]][arr[0]] = true;
         }
-        boolean[] visited = new boolean[array[0] + 1];
-        StringBuilder stringBuilder = new StringBuilder();
-        dfs(array[2], array[0], visited, stringBuilder, board);
+        visited = new boolean[array[0] + 1];
+        stringBuilder = new StringBuilder();
+        dfs(array[2], array[0]);
         stringBuilder.deleteCharAt(stringBuilder.length() - 1);
         visited = new boolean[array[0] + 1];
         stringBuilder.append("\n");
-        bfs(array[2], array[0], visited, board, stringBuilder);
+        bfs(array[2], array[0]);
         return stringBuilder.toString();
     }
 
-    private static void bfs(int start, int n, boolean[] visited, boolean[][] board, StringBuilder stringBuilder) {
+    private static void bfs(int start, int n) {
         Deque<Integer> deque = new LinkedList<>();
         deque.add(start);
         visited[start] = true;
@@ -54,12 +58,12 @@ public class Main1260 {
 
     }
 
-    private static void dfs(int start, int n, boolean[] visited, StringBuilder stringBuilder, boolean[][] board) {
+    private static void dfs(int start, int n) {
         visited[start] = true;
         stringBuilder.append(start).append(" ");
         for (int i = 1; i <= n; i++) {
             if (!visited[i] && board[start][i]) {
-                dfs(i, n, visited, stringBuilder, board);
+                dfs(i, n);
             }
         }
 
