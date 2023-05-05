7d622c61fe093fac12f9668db7a42adfae83f891
yeji.jo <choyeji1591@gmail.com>
2023-05-05 19:25:22 +0900

    test : class3 토마토 테스트케이스 추가

---
+++ src/test/java/org/backjoon/class3/yeji/Main7569Test.java
@@ -0,0 +1,50 @@
+package org.backjoon.class3.yeji;
+
+import static org.backjoon.class3.yeji.Main7569.solution;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import org.backjoon.common.TestFileUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName( "토마토 - 예지" )
+class Main7569Test {
+
+    @Test
+    @DisplayName( "토마토 케이스1" )
+    void test_case_1 () throws IOException {
+        // given
+        BufferedReader reader = TestFileUtil.getReader( this.getClass(), "testcase/class3/7569/test_case1.txt" );
+        // when
+        int solution = solution( reader );
+
+        // then
+        assertEquals( -1, solution );
+    }
+
+    @Test
+    @DisplayName( "토마토 케이스2" )
+    void test_case_2 () throws IOException {
+        // given
+        BufferedReader reader = TestFileUtil.getReader( this.getClass(), "testcase/class3/7569/test_case2.txt" );
+        // when
+        int solution = solution( reader );
+
+        // then
+        assertEquals( 4, solution );
+    }
+
+    @Test
+    @DisplayName( "토마토 케이스3" )
+    void test_case_3 () throws IOException {
+        // given
+        BufferedReader reader = TestFileUtil.getReader( this.getClass(), "testcase/class3/7569/test_case3.txt" );
+        // when
+        int solution = solution( reader );
+
+        // then
+        assertEquals( 0, solution );
+    }
+}
