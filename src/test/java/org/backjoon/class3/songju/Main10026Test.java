301248e03cb4c9b06b8d1f54e2c3b5c324830813
songjuAhn <ggb06104@naver.com>
2023-05-06 12:09:28 +0900

    test : class3 적록색약 테스트케이스 추가

---
+++ src/test/java/org/backjoon/class3/songju/Main10026Test.java
@@ -0,0 +1,21 @@
+package org.backjoon.class3.songju;
+
+import org.backjoon.common.TestFileUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("적록색약 - 송주")
+class Main10026Test {
+
+    @Test
+    @DisplayName( "적록색약 테스트 케이스 1" )
+    void test_case_1 () throws Exception {
+        BufferedReader reader = TestFileUtil.getReader( this.getClass(), "testcase/class3/10026/test_case1.txt");
+        String solution = Main10026.solution( reader );
+        assertEquals( "4 3", solution );
+    }
+}
