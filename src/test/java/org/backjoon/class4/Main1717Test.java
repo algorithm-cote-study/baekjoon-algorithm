3656efe55395397f56d73bf3f4b85a7e0e6ae7c1
yeji.jo <choyeji1591@gmail.com>
2023-05-10 11:18:13 +0900

    test : union&find 집합의 표현

---
+++ src/test/java/org/backjoon/class4/Main1717Test.java
@@ -0,0 +1,29 @@
+package org.backjoon.class4;
+
+import static org.backjoon.class4.Main1717.solution;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import org.backjoon.common.TestFileUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("집합의 표현 - 예지")
+class Main1717Test {
+
+    @Test
+    @DisplayName("집합의 표현 케이스1")
+    void test_case_1() throws IOException {
+        // given
+        BufferedReader reader = TestFileUtil.getReader(this.getClass(), "testcase/class4/1717/test_case1.txt");
+        // when
+        String solution = solution(reader);
+        // then
+        assertEquals("""
+            NO
+            NO
+            YES
+            """.trim(), solution.trim());
+    }
+}
