b1a831343ff9944d902aef47e29c6c55faa415dc
yeji.jo <choyeji1591@gmail.com>
2023-05-15 22:05:24 +0900

    feat : 피보나치 수 2 풀이 추가

---
+++ src/main/java/org/backjoon/class5/Main2748.java
@@ -0,0 +1,36 @@
+package org.backjoon.class5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// DP : 피보나치 수 2
+public class Main2748 {
+
+
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.print(solution(reader));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static long solution(BufferedReader br) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+
+        if (n == 0 || n == 1) return n;
+
+        long first = 0;
+        long second = 1;
+        for (int i = 2; i <= n; i++) {
+            long temp = first + second;
+            first = second;
+            second = temp;
+        }
+
+        return second;
+    }
+
+
+}
