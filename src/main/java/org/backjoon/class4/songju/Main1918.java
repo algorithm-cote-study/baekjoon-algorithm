738acfa7e4ef8cdc6811fd63fe74789921148e5e
songjuAhn <ggb06104@naver.com>
2023-05-15 15:32:25 +0900

    fix: class3 후위 표기식 성능을 위한 Stack -> Deque로 변경

--- src/main/java/org/backjoon/class4/songju/Main1918.java
+++ src/main/java/org/backjoon/class4/songju/Main1918.java
@@ -3,7 +3,8 @@ package org.backjoon.class4.songju;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 //후위 표기식
 public class Main1918 {
@@ -19,26 +20,26 @@ public class Main1918 {
     public static String solution(BufferedReader br) throws IOException {
         StringBuilder result = new StringBuilder();
         String expression = br.readLine();
-        Stack<Character> stack = new Stack<>();
+        Deque<Character> deque = new ArrayDeque<>();
         for (int i = 0; i < expression.length(); i++) {
             char exp = expression.charAt(i);
             switch (exp) {
                 case '+', '-','*','/' -> {
-                    while (!stack.isEmpty() && checkPriority(stack.peek()) >= checkPriority(exp))
-                        result.append(stack.pop());
-                    stack.push(exp);
+                    while (!deque.isEmpty() && checkPriority(deque.peekLast()) >= checkPriority(exp))
+                        result.append(deque.pollLast());
+                    deque.offer(exp);
                 }
-                case '(' -> stack.push(exp);
+                case '(' -> deque.offer(exp);
                 case ')' -> {
-                    while (!stack.isEmpty() && stack.peek() != '(')
-                        result.append(stack.pop());
-                    stack.pop();
+                    while (!deque.isEmpty() && deque.peekLast() != '(')
+                        result.append(deque.pollLast());
+                    deque.pollLast();
                 }
                 default -> result.append(exp);
             }
         }
-        while(!stack.isEmpty()) {
-            result.append(stack.pop());
+        while(!deque.isEmpty()) {
+            result.append(deque.pollLast());
         }
         return result.toString();
     }
