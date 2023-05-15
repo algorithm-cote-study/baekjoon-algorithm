67000a8b58168889a129c88cabc5c72d736e0888
songjuAhn <ggb06104@naver.com>
2023-05-15 15:32:58 +0900

    fix: 테스트 케이스 경로 변경

--- src/test/java/org/backjoon/class4/songju/Main1918Test.java
+++ src/test/java/org/backjoon/class4/songju/Main1918Test.java
@@ -15,7 +15,7 @@ class Main1918Test {
     @Test
     @DisplayName( "후위 표기식 테스트 케이스 1" )
     void test_case_1 () throws Exception {
-        BufferedReader reader = TestFileUtil.getReader( this.getClass(), "testcase/class3/1018/test_case1.txt");
+        BufferedReader reader = TestFileUtil.getReader( this.getClass(), "testcase/class3/1918/test_case1.txt");
         String solution = Main1918.solution( reader );
         assertEquals( "ABC+*", solution );
     }
@@ -23,7 +23,7 @@ class Main1918Test {
     @Test
     @DisplayName( "후위 표기식 테스트 케이스 2" )
     void test_case_2 () throws Exception {
-        BufferedReader reader = TestFileUtil.getReader( this.getClass(), "testcase/class3/1018/test_case2.txt");
+        BufferedReader reader = TestFileUtil.getReader( this.getClass(), "testcase/class3/1918/test_case2.txt");
         String solution = Main1918.solution( reader );
         assertEquals( "AB+", solution );
     }
@@ -31,7 +31,7 @@ class Main1918Test {
     @Test
     @DisplayName( "후위 표기식 테스트 케이스 3" )
     void test_case_3 () throws Exception {
-        BufferedReader reader = TestFileUtil.getReader( this.getClass(), "testcase/class3/1018/test_case3.txt");
+        BufferedReader reader = TestFileUtil.getReader( this.getClass(), "testcase/class3/1918/test_case3.txt");
         String solution = Main1918.solution( reader );
         assertEquals( "ABC*+", solution );
     }
@@ -39,7 +39,7 @@ class Main1918Test {
     @Test
     @DisplayName( "후위 표기식 테스트 케이스 4" )
     void test_case_4 () throws Exception {
-        BufferedReader reader = TestFileUtil.getReader( this.getClass(), "testcase/class3/1018/test_case4.txt");
+        BufferedReader reader = TestFileUtil.getReader( this.getClass(), "testcase/class3/1918/test_case4.txt");
         String solution = Main1918.solution( reader );
         assertEquals( "ABC*+DE/-", solution );
     }
