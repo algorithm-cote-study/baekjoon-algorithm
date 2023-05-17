f9141717ec88aadb35f70ac5eee9b218e4d822d6
yeji.jo <choyeji1591@gmail.com>
2023-05-17 14:47:59 +0900

    feat: 퇴사 2 시간초과

--- src/main/java/org/backjoon/class5/Main15486.java
+++ src/main/java/org/backjoon/class5/Main15486.java
@@ -4,12 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
 
 // 퇴사2
 public class Main15486 {
-
 
     public static void main(String[] args) {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
@@ -21,39 +18,23 @@ public class Main15486 {
 
     static int solution(BufferedReader br) throws IOException {
         int n = Integer.parseInt(br.readLine());
-
-        Queue<Schedule> queue = new LinkedList<>();
         int[] result = new int[n + 1];
 
-        for (int i = 1; i <= n; i++) {
+        for (int day = 1; day <= n; day++) {
             int[] schedules = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            queue.offer(new Schedule(i, schedules[0], schedules[1]));
-        }
+            int requiredDays = schedules[0];
+            int cost = schedules[1];
 
-        while (!queue.isEmpty()) {
-            Schedule poll = queue.poll();
-            int cur = poll.day + poll.requiredDays - 1;
-            for (int i = cur; i < n + 1; i++) {
-                if (cur < n + 1) {
-                    result[i] = Math.max(result[i], poll.cost + result[cur - poll.requiredDays]);
+            int cur = day + requiredDays - 1;
+            if (cur < n + 1 && (result[cur] < cost + result[cur - requiredDays])) {
+                for (int i = cur; i < n + 1; i++) {
+                    result[i] = Math.max(result[i], cost + result[cur - requiredDays]);
                 }
             }
 
         }
+
         return result[n];
     }
 
-    static class Schedule {
-
-        int day;
-        int requiredDays;
-        int cost;
-
-        public Schedule(int day, int requiredDays, int cost) {
-            this.day = day;
-            this.requiredDays = requiredDays;
-            this.cost = cost;
-        }
-
-    }
 }
