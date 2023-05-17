b38c8624d64c8121b470cdc218151f2f3d888283
yeji.jo <choyeji1591@gmail.com>
2023-05-17 15:04:04 +0900

    fix : 퇴사2 풀이 추가 (통과)

--- src/main/java/org/backjoon/class5/Main15486.java
+++ src/main/java/org/backjoon/class5/Main15486.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
 
 // 퇴사2
 public class Main15486 {
@@ -18,23 +20,39 @@ public class Main15486 {
 
     static int solution(BufferedReader br) throws IOException {
         int n = Integer.parseInt(br.readLine());
-        int[] result = new int[n + 1];
-
-        for (int day = 1; day <= n; day++) {
+        int[] result = new int[n + 2];
+        Deque<Schedule> queue = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
             int[] schedules = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            int requiredDays = schedules[0];
-            int cost = schedules[1];
-
-            int cur = day + requiredDays - 1;
-            if (cur < n + 1 && (result[cur] < cost + result[cur - requiredDays])) {
-                for (int i = cur; i < n + 1; i++) {
-                    result[i] = Math.max(result[i], cost + result[cur - requiredDays]);
-                }
-            }
-
+            queue.offer(new Schedule(i, schedules[0], schedules[1]));
         }
 
-        return result[n];
+        int i = n;
+        while (!queue.isEmpty()) {
+            Schedule cur = queue.pollLast();
+            if (i + cur.requiredDays > n + 1) {
+                result[i] = result[i + 1];
+            } else {
+                result[i] = Math.max(result[i + 1], cur.cost + result[i + cur.requiredDays]);
+            }
+
+            i--;
+        }
+
+        return result[1];
+    }
+
+    static class Schedule {
+
+        int day;
+        int requiredDays;
+        int cost;
+
+        public Schedule(int day, int requiredDays, int cost) {
+            this.day = day;
+            this.requiredDays = requiredDays;
+            this.cost = cost;
+        }
     }
 
 }
