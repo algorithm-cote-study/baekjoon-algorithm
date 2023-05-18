505be403ac25a78fc024e95353212253aefc2fc8
songjuAhn <ggb06104@naver.com>
2023-05-19 08:49:08 +0900

    test : class3 뱀과 사다리 게임 테스트케이스 추가

---
+++ src/test/java/org/backjoon/class3/songju/Main16928Test.java
@@ -0,0 +1,29 @@
+package org.backjoon.class3.songju;
+
+import org.backjoon.common.TestFileUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("뱀과 사다리 게임 - 송주")
+class Main16928Test {
+
+    @Test
+    @DisplayName( "뱀과 사다리 게임 테스트 케이스 1" )
+    void test_case_1 () throws Exception {
+        BufferedReader reader = TestFileUtil.getReader( this.getClass(), "testcase/class3/16928/test_case1.txt");
+        int solution = Main16928.solution( reader );
+        assertEquals( 3, solution );
+    }
+
+    @Test
+    @DisplayName( "뱀과 사다리 게임 테스트 케이스 1" )
+    void test_case_2 () throws Exception {
+        BufferedReader reader = TestFileUtil.getReader( this.getClass(), "testcase/class3/16928/test_case2.txt");
+        int solution = Main16928.solution( reader );
+        assertEquals( 5, solution );
+    }
+}
