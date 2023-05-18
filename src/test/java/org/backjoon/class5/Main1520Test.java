33d2270f83fedaac31d71c662be36eb036926bc4
yeji.jo <choyeji1591@gmail.com>
2023-05-18 14:17:01 +0900

    test : 내리막길 테스트 추가

---
+++ src/test/java/org/backjoon/class5/Main1520Test.java
@@ -0,0 +1,28 @@
+package org.backjoon.class5;
+
+import static org.backjoon.class5.Main1520.solution;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import org.backjoon.common.TestFileUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("내리막길 - 예지")
+class Main1520Test {
+
+    @Test
+    @DisplayName("내리막길 케이스1")
+    void test_case_1() throws IOException {
+        // given
+        BufferedReader reader = TestFileUtil.getReader(this.getClass(), "testcase/class5/1520/test_case1.txt");
+
+        // when
+        long solution = solution(reader);
+
+        // then
+        assertEquals(3, solution);
+    }
+
+}
