4f223f5629a4a804c1c45ef6fa55e95f280b47ed
songjuAhn <ggb06104@naver.com>
2023-05-25 16:27:12 +0900

    test : 회전하는 큐 테스트케이스 추가

---
+++ src/test/java/org/backjoon/class6/songju/Main1021Test.java
@@ -0,0 +1,45 @@
+package org.backjoon.class6.songju;
+
+import org.backjoon.common.TestFileUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("회전하는 큐 - 송주")
+class Main1021Test {
+
+    @Test
+    @DisplayName( "회전하는 큐 테스트 케이스 1" )
+    void test_case_1 () throws Exception {
+        BufferedReader reader = TestFileUtil.getReader( this.getClass(), "testcase/class6/1021/test_case1.txt");
+        int solution = Main1021.solution( reader );
+        assertEquals( 0, solution );
+    }
+
+    @Test
+    @DisplayName( "회전하는 큐 테스트 케이스 2" )
+    void test_case_2 () throws Exception {
+        BufferedReader reader = TestFileUtil.getReader( this.getClass(), "testcase/class6/1021/test_case2.txt");
+        int solution = Main1021.solution( reader );
+        assertEquals( 8, solution );
+    }
+
+    @Test
+    @DisplayName( "회전하는 큐 테스트 케이스 3" )
+    void test_case_3 () throws Exception {
+        BufferedReader reader = TestFileUtil.getReader( this.getClass(), "testcase/class6/1021/test_case3.txt");
+        int solution = Main1021.solution( reader );
+        assertEquals( 59, solution );
+    }
+
+    @Test
+    @DisplayName( "회전하는 큐 테스트 케이스 4" )
+    void test_case_4 () throws Exception {
+        BufferedReader reader = TestFileUtil.getReader( this.getClass(), "testcase/class6/1021/test_case4.txt");
+        int solution = Main1021.solution( reader );
+        assertEquals( 14, solution );
+    }
+}
