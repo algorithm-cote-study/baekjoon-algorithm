94ac9c59d8e33fd0f28c04ec41bdb508b6383584
yeji.jo <choyeji1591@gmail.com>
2023-05-27 20:45:48 +0900

    fix : 흙길보수하기 Puddle객체 생성

--- src/main/java/org/backjoon/class6/yeji/Main1911.java
+++ src/main/java/org/backjoon/class6/yeji/Main1911.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -22,32 +23,27 @@ public class Main1911 {
 
     static int solution(BufferedReader br) throws IOException {
         int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        List<int[]> puddles = new ArrayList<>();
+        List<Puddle> puddles = new ArrayList<>();
 
         for (int i = 0; i < input[0]; i++) {
-            puddles.add(Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray());
+            int[] puddle = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            puddles.add(new Puddle(puddle[0], puddle[1]));
         }
 
-        puddles.sort(((o1, o2) -> {
-            if (o1[0] == o2[0]) {
-                return o1[1] - o2[1];
-            }
-            return o1[0] - o2[0];
-        }));
-
+        Collections.sort(puddles);
         return puddlesCount(puddles, input[1]);
     }
 
-    private static int puddlesCount(List<int[]> puddles, int size) {
+    private static int puddlesCount(List<Puddle> puddles, int size) {
         int result = 0;
         int range = 0;
-        for (int[] puddle : puddles) {
-            if (puddle[0] > range) {
-                range = puddle[0];
+        for (Puddle puddle : puddles) {
+            if (puddle.start > range) {
+                range = puddle.start;
             }
 
-            if (puddle[1] > range) {
-                while (puddle[1] > range) {
+            if (puddle.end > range) {
+                while (puddle.end > range) {
                     range += size;
                     result++;
                 }
@@ -55,6 +51,26 @@ public class Main1911 {
         }
 
         return result;
+    }
+
+    static class Puddle implements Comparable<Puddle> {
+
+        private final int start;
+        private final int end;
+
+        Puddle(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        @Override
+        public int compareTo(Puddle puddle) {
+            if (this.start == puddle.start) {
+                return this.end - puddle.end;
+            }
+            return this.start - puddle.start;
+        }
+
     }
 
 
