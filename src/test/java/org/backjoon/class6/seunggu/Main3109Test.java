93ec710a0eb0799332e80c8e9053ce4e520cc233
leeseunggu <leesg6107@gmail.com>
2023-05-28 23:49:16 +0900

    test: 빵집 테스트 케이스 추가

---
+++ src/test/java/org/backjoon/class6/seunggu/Main3109Test.java
@@ -0,0 +1,36 @@
+package org.backjoon.class6.seunggu;
+
+import static org.backjoon.class6.seunggu.Main2195.solution;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.BufferedReader;
+import org.backjoon.common.TestFileUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("빵집 - 승구")
+class Main3109Test {
+
+    @Test
+    @DisplayName("문자열 복사 테스트 케이스 1")
+    void test_case_1 () throws Exception {
+        // given
+        BufferedReader reader = TestFileUtil.getReader( this.getClass(), "testcase/class6/3109/test_case1.txt" );
+        // when
+        int solution = solution( reader );
+        // then
+        assertEquals( 2, solution );
+    }
+
+    @Test
+    @DisplayName("문자열 복사 테스트 케이스 2")
+    void test_case_2 () throws Exception {
+        // given
+        BufferedReader reader = TestFileUtil.getReader( this.getClass(), "testcase/class6/3109/test_case2.txt" );
+        // when
+        int solution = solution( reader );
+        // then
+        assertEquals( 5, solution );
+    }
+
+}
