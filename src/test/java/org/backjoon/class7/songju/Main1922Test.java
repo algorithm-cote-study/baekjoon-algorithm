9b6fc0fdb97343529014933ba0139d01c56abeb6
songjuAhn <ggb06104@naver.com>
2023-05-30 16:50:07 +0900

    test : 네트워크 연결 테스트케이스 추가

---
+++ src/test/java/org/backjoon/class7/songju/Main1922Test.java
@@ -0,0 +1,21 @@
+package org.backjoon.class7.songju;
+
+import org.backjoon.common.TestFileUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("네트워크 연결 - 송주")
+class Main1922Test {
+
+    @Test
+    @DisplayName( "네트워크 연결 테스트 케이스 1" )
+    void test_case_1 () throws Exception {
+        BufferedReader reader = TestFileUtil.getReader( this.getClass(), "testcase/class7/1922/test_case1.txt");
+        int solution = Main1922.solution( reader );
+        assertEquals( 23, solution );
+    }
+}
