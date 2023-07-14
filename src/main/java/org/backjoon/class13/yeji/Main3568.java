6cb0578d5c16bbc8503842374234366cd9811799
cyeji <choyeji1591@gmail.com>
2023-07-14 11:52:13 +0900

    feat : iSharp 풀이 추가

---
+++ src/main/java/org/backjoon/class13/yeji/Main3568.java
@@ -0,0 +1,64 @@
+package org.backjoon.class13.yeji;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Main3568 {
+
+    static String solution(BufferedReader br) throws IOException {
+        String[] iSharp = br.readLine().split(",");
+        StringBuilder answer = new StringBuilder();
+        String start = "";
+        for (int i = 0; i < iSharp.length - 1; i++) {
+            if (i == 0) {
+                start = iSharp[i].split(" ")[0];
+            }
+            String variable = iSharp[i].split(" ")[1];
+            answer.append(start).append(findVariable(variable)).append(";").append("\n");
+        }
+        if (start.isEmpty()) {
+            start = iSharp[0].split(" ")[0];
+        }
+        answer.append(start).append(findVariable(iSharp[iSharp.length - 1].split(" ")[1].replace(";", ""))).append(";");
+        return answer.toString();
+    }
+
+    private static String findVariable(String variable) {
+        Deque<Character> stack = new ArrayDeque<>();
+        for (int i = 0; i < variable.length(); i++) {
+            stack.push(variable.charAt(i));
+        }
+        StringBuilder start = new StringBuilder();
+        StringBuilder end = new StringBuilder();
+        while (!stack.isEmpty()) {
+            char cur = stack.peek();
+            if (Character.isAlphabetic(cur)) {
+                break;
+            }
+            if (cur == ']') {
+                start.append("[]");
+                stack.pop();
+                stack.pop();
+            } else {
+                start.append(stack.pop());
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            end.append(stack.pop());
+        }
+
+        return start.append(" ").toString() + end.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.print(solution(reader));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
