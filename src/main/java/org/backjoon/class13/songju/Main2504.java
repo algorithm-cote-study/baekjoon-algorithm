13b6b978250c2d570017a86b06f7856467fcb8b7
songjuAhn <ggb06104@naver.com>
2023-07-16 17:59:49 +0900

    feat : 괄호의 값 풀이 추가

---
+++ src/main/java/org/backjoon/class13/songju/Main2504.java
@@ -0,0 +1,55 @@
+package org.backjoon.class13.songju;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+// 괄호의 값
+public class Main2504 {
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println(solution(br));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static int solution(BufferedReader br) throws IOException {
+        Stack<Character> stack = new Stack<>();
+        char[] brackets = br.readLine().toCharArray();
+        int result = 0;
+        int val = 1;
+        for (int i = 0; i < brackets.length; i++) {
+            char c = brackets[i];
+            if (c == '(') {
+                stack.push(c);
+                val *= 2;
+            } else if (c == '[') {
+                stack.push(c);
+                val *= 3;
+            } else if (c == ')') {
+                if (stack.isEmpty() || stack.peek() != '(') {
+                    result = 0;
+                    break;
+                } else if (brackets[i - 1] == '(') {
+                    result += val;
+                }
+                stack.pop();
+                val /= 2;
+            } else if (c == ']') {
+                if (stack.isEmpty() || stack.peek() != '[') {
+                    result = 0;
+                    break;
+                } else if (brackets[i - 1] == '[') {
+                    result += val;
+                }
+                stack.pop();
+                val /= 3;
+            }
+        }
+
+        return !stack.isEmpty() ? 0 : result;
+    }
+}
